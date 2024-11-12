ac7db670c859ee3d0c9cbcfd2cb628a0b2bf530f
shalusingh <shalusiingh94@gmail.com>
2024-11-12 16:56:23 +0530

    make it better

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,24 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Being passionate for design and developing, i have completed more than 15 projects.
+            As a front-end developer and UX/UI designer,I specialize in crafting seamless and engaging user experiences. 
+            I bring design and functionality together by creating visually appealing interfaces that are also highly 
+            functional and user-friendly.
+          </p>
+          <p className="hero--section-description">
+          My skills bridge the gap between aesthetic design and technical implementation, ensuring that
+           the end product is both attractive and accessible for users.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
